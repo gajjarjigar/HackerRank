e1d5321ece680752cab13c8033a9194408a3cdba
Jigar Gajjar <38879584+gajjarjigar@users.noreply.github.com>
2018-09-07 15:42:57 +0530

    Create python-string-formatting.py

---
+++ Python/Strings/python-string-formatting.py
@@ -0,0 +1,8 @@
+def print_formatted(number):
+    width = len(str(bin(number))[2:])
+    for i in range(1,number+1):
+        print('{} {} {} {}'.format(str(i).rjust(width), str(oct(i))[2:].rjust(width),str(hex(i))[2:].upper().rjust(width), str(bin(i))[2:].rjust(width)))
+    
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
